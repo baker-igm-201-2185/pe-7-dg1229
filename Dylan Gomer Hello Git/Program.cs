1088494247ec200186e81614251883457d9ae4a6
dg1229 <dg1229@IGM2570-29>
2019-02-06 11:17:07 -0500

    Created new project

---
+++ Dylan Gomer Hello Git/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dylan_Gomer_Hello_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
