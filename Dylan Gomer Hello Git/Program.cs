fb5c7c54deb71f507d9803ae2d07a023b231d7f0
dg1229 <dg1229@IGM2570-29>
2019-02-06 11:23:22 -0500

    Take a user number and multiply it by 10.

--- Dylan Gomer Hello Git/Program.cs
+++ Dylan Gomer Hello Git/Program.cs
@@ -10,7 +10,16 @@ namespace Dylan_Gomer_Hello_Git
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello Git World");
+            Console.WriteLine("Give a number: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            number = number * 10;
+
+            Console.WriteLine("Your number times 10 is {0}", number);
+
         }
     }
 }
